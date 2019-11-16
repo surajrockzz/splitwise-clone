481bdc95b0c515bd2d4a1f9697861a1e094aea90
Suraj <suraj.p@reportgarden.com>
2019-11-16 23:59:48 +0530

    added menu component but buttons not working

---
+++ src/Layouts/DrawerComponent.js
@@ -0,0 +1,67 @@
+import React, { memo } from 'react';
+import { makeStyles } from '@material-ui/styles';
+import { Drawer, Divider, List, ListItemText } from '@material-ui/core';
+import ListItem from '@material-ui/core/List';
+import ListItemIcon from '@material-ui/icons/List';
+import IconButton from '@material-ui/core/IconButton';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
+const drawerWidth = 240;
+const useStyles = makeStyles({
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    drawerHeader: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: "0 1px",
+        justifyContent: 'flex-end',
+    },
+});
+const DrawerComponent = ({ handleDrawerClose, open }) => {
+    const classes = useStyles();
+    return (
+        <Drawer
+            className={classes.drawer}
+            variant="persistent"
+            anchor="left"
+            open={open}
+            classes={{
+                paper: classes.drawerPaper,
+            }}
+        >
+            <div className={classes.drawerHeader}>
+                <IconButton onClick={handleDrawerClose}>
+                    {'ltr' === 'trr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                </IconButton>
+            </div>
+            <Divider />
+            <List>
+                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+            <Divider />
+            <List>
+                {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+        </Drawer>
+    )
+}
+
+export default memo(DrawerComponent);
