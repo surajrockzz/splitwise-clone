8b72810a53427881edc20de90adbc01602e5632c
Suraj <suraj.p@reportgarden.com>
2019-12-09 23:42:46 +0530

    added routes

---
+++ src/Routes/routes.js
@@ -0,0 +1,33 @@
+const firstSection = [
+    {
+        name: 'Dashboard',
+        path: '/dashboard'
+    },
+    {
+        name: 'Rececnt Activity',
+        path: '/activity'
+    },
+    {
+        name: 'All Expenses',
+        path: '/all'
+    },
+
+];
+
+const secondSection = [
+    {
+        name: 'Groups',
+        path: '/groups'
+    },
+    {
+        name: 'Friends',
+        path: '/friends'
+    },
+    {
+        name: 'Invite',
+        path: '/invite'
+    },
+];
+
+
+export {secondSection, firstSection};
