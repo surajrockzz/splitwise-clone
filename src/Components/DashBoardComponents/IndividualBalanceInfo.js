f34a82d21dedca622f5f44244a56da00558277cb
Suraj <suraj.p@reportgarden.com>
2019-12-13 09:20:44 +0530

    added base card structure todo refactor

--- src/Components/DashBoardComponents/IndividualBalanceInfo.js
+++ src/Components/DashBoardComponents/IndividualBalanceInfo.js
@@ -1,6 +1,22 @@
 import React from 'react'
-import { Grid, Divider } from '@material-ui/core';
+import { Grid, Divider, Avatar } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles({
+    pId:{
+        margin: 0,
+        padding: '1% 0 1% 0'
+    },
+    pLeftYo: {
+        paddingLeft: '14%' 
+    },
+    pLeftYao: {
+        paddingLeft: '8%' 
+    },
+
+});
 const IndividualBalanceInfo = ({ name, text }) => {
+    const classes = useStyles();
     return (
         <>
             <Grid container alignContent="flex-end">
@@ -17,21 +33,43 @@ const IndividualBalanceInfo = ({ name, text }) => {
             </Grid>
             <Divider />
             <Grid container xs={12}>
-                <Grid
-                    container
-                    direction="column"
-                    justify="flex-start"
-                    alignItems="flex-start"
-                    xs={6}
-                ><p>suraj</p><p>suraj</p><p>suraj</p></Grid>
-                <Grid
-                    container
-                    direction="column"
-                    justify="flex-start"
-                    alignItems="flex-start"
-                    xs={6}
-                ><p>suraj</p><p>suraj</p><p>suraj</p></Grid>
+            <Grid
+                container
+                item
+                direction="column"
+                justify="flex-start"
+                alignItems="flex-end"
+                xs={6}
+            >
+                <Grid container xs={12} className = {classes.pLeftYo}>
+                    <Grid container item xs={1} alignItems="center">
+                        <Avatar>RK</Avatar>
+                    </Grid>
+                    <Grid container item xs={6} direction = "column" alignItems="flex-start" justify="flex-start">
+                        <p className = {classes.pId}>Rama Krishna</p>
+                        <p className = {classes.pId}>You Owe INR13.33</p>
+                    </Grid>
+                </Grid>
             </Grid>
+            <Grid
+                container
+                item
+                direction="column"
+                justify="flex-start"
+                alignItems="flex-end"
+                xs={6}
+            >
+                <Grid container xs={12} className = {classes.pLeftYo}>
+                    <Grid container item xs={1} alignItems="center">
+                        <Avatar>RK</Avatar>
+                    </Grid>
+                    <Grid container item xs={6} direction = "column" alignItems="flex-start" justify="flex-start">
+                        <p className = {classes.pId}>Rama Krishna</p>
+                        <p className = {classes.pId}>You Owe INR13.33</p>
+                    </Grid>
+                </Grid>
+            </Grid>
+         </Grid>   
         </>
     )
 }
