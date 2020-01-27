fea7442216a72dc0133254cf61ed2c2b1196c362
Suraj <suraj.p@reportgarden.com>
2020-01-27 19:27:22 +0530

    added proper owe and owned in dashboard

--- src/Components/DashBoardComponents/IndividualBalanceInfo.js
+++ src/Components/DashBoardComponents/IndividualBalanceInfo.js
@@ -1,50 +1,13 @@
 import React from 'react';
-import { Grid, Avatar } from '@material-ui/core';
-import { makeStyles } from '@material-ui/styles';
+import { Grid } from '@material-ui/core';
+import InformationContainer from './InformationContainer'
 
-const useStyles = makeStyles({
-    pId: {
-        margin: 0,
-        padding: '1% 0 1% 0'
-    },
-    pLeftYo: {
-        paddingLeft: '14%'
-    },
-    pLeftYao: {
-        paddingLeft: '8%'
-    },
-    grid: {
-        padding: '1%'
-    }
-});
+const IndividualBalanceInfo = ({users}) => {
 
-const IndividualBalanceInfo = (props) => {
-    const classes = useStyles();
     return(
-        <Grid container xs={12} className={classes.grid}>
-                <Grid container item direction="column" justify="flex-start" alignItems="flex-end" xs={6}>
-                    <Grid container xs={12} className={classes.pLeftYo}>
-                        <Grid container item xs={1} alignItems="center">
-                            <Avatar>RK</Avatar>
-                        </Grid>
-                        <Grid container item xs={6} direction="column" alignItems="flex-start" justify="flex-start">
-                            <p className={classes.pId}>Rama Krishna</p>
-                            <p className={classes.pId}>You Owe INR13.33</p>
-                        </Grid>
-                    </Grid>
-                </Grid>
-                <Grid container item direction="column" justify="flex-start" alignItems="flex-end" xs={6}>
-                    <Grid container xs={12} className={classes.pLeftYo}>
-                        <Grid container item xs={1} alignItems="center">
-                            <Avatar>VK</Avatar>
-                        </Grid>
-                        <Grid container item xs={6} direction="column" alignItems="flex-start" justify="flex-start">
-                            <p className={classes.pId}>Vinay Kumar</p>
-                            <p className={classes.pId}>You Owe INR13.33</p>
-                        </Grid>
-                    </Grid>
-                </Grid>
-            </Grid>
+        <Grid  container xs={6} direction = "column" >
+            {users.map((user) => <InformationContainer name = {user.name} amount={user.amount}/> )}
+        </Grid>
     )
 }
 
--- src/Components/DashBoardComponents/IndividualBalanceInfoContainer.js
+++ src/Components/DashBoardComponents/IndividualBalanceInfoContainer.js
@@ -2,25 +2,48 @@ import React from 'react'
 import { Grid, Divider } from '@material-ui/core';
 import IndividualBalanceInfo from './IndividualBalanceInfo';
 
-const IndividualBalanceInfoContainer = ({ name, text }) => {
+const IndividualBalanceInfoContainer = () => {
+
+    let owedUsers = [
+        {
+            name: 'suraj penugonda',
+            amount: 'You owe Rs100'
+        },
+        {
+            name: 'Disco Raja',
+            amount: 'You owe Rs200'
+        }
+    ]
+
+    let ownedUsers = [
+        {
+            name: 'Ala vaikuntapuramplo',
+            amount: 'You owned Rs1000'
+        },
+        {
+            name: 'Sarileru nekavvaru',
+            amount: 'You owned Rs2000'
+        },{
+            name: 'ENtha manchivaduvura',
+            amount: 'You owned Rs3000'
+        }
+    ]
+
     return (
         <>
-            <Grid container alignContent="flex-end">
-                <Grid container xs={6} >
-                    <Grid item xs={6}>
-                        <h5>You Owe</h5>
-                    </Grid>
+            <Grid container direction = "row" spacing={2}>
+                <Grid  item xs={6} >
+                    <h5>You Owe</h5>
                 </Grid>
-                <Grid container xs={6}>
-                    <Grid item xs={6}>
+                <Grid  item xs={6}>
                         <h5>You are owned</h5>
-                    </Grid>
                 </Grid>
             </Grid>
             <Divider />
-            <IndividualBalanceInfo/>
-            <IndividualBalanceInfo/>
-            <IndividualBalanceInfo/>
+            <Grid container direction="row" alignItems="flex-start">
+                <IndividualBalanceInfo users = {owedUsers}/>
+                <IndividualBalanceInfo users = {ownedUsers}/>
+            </Grid>
         </>
     )
 }
