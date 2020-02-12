a7ec1f9bb1038de558e0be726a5a8c1e320f646a
Suraj <suraj.p@reportgarden.com>
2020-02-12 21:56:02 +0530

    added proper css for login module

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,49 +2,41 @@ import React from 'react';
 import { Paper, TextField, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 const useStyles = makeStyles({
-  getDown: {
-    marginTop: '10%',
-    marginLeft: '30%',
-    marginRight: '20%'
-  },
-  flexDirectionColumn: {
-    display: 'grid',
-    gridTemplateColumns: '40px 50px auto 100px 100px 40px',
-    gridTemplateRows: '25% 100px 20%',
-    gridRowGap: '10px',
-    gridColumnGap: '5px',
-  }
 });
+
+
+
+
 
 const Login = () => {
   const classes = useStyles();
-  return (<div className={classes.getDown}>
-    <Paper variant="outlined" style={{ height: 223 }}>
-      <div className={classes.flexDirectionColumn}>
-        <div className="inputDiv">
-        <TextField
-          id="outlined-username-input"
-          label="Username"
-          type="text"
-          variant="outlined"
-        />
-        <TextField
-          id="outlined-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-          variant="outlined"
-        />
-        </div>
-        <div className = "buttonsDiv">
-          <Button variant="contained" color="primary">
+  return (<div>
+    <Paper variant="outlined" className="paperDiv">
+      <div className="loginContainer">
+        <img className="logoDiv" src="https://via.placeholder.com/150"/>
+        <div className="fieldsDiv">
+          <TextField
+            id="outlined-username-input"
+            label="Username"
+            type="text"
+            variant="outlined"
+            className= "inputField"
+          />
+          <TextField
+            id="outlined-password-password"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+            variant="outlined"
+            className= "passwordField"
+          />
+          <Button variant="contained" color="primary" className="loginButton">
             Login
           </Button>
-          <Button variant="contained" color="primary">
+          <Button variant="contained" color="primary" className="registerButton">
             Register
-            </Button>
-        </div>
-
+          </Button>
+          </div>
       </div>
     </Paper>
 
